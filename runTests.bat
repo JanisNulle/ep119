90f1fcbbc0e62ff7324738c465abb23dd0606fc4
JanisNulle <janis.nulle@squalio.com>
2016-01-19 16:15:12 +0200

    Bat fix 5

--- runTests.bat
+++ runTests.bat
@@ -1,10 +1,3 @@
 cd %~dp0
 java -cp ".;lib/*;bin/" org.testng.TestNG testng.xml
-
-git checkout master
-git add .
-git commit -m "Automated test run"
-git remote set-url origin https://github.com/JanisNulle/ep119.git
-git push -u origin master
-pause
 exit /b 0

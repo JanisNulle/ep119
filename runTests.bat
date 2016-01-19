4b8c35093e820d913a44998909c9e0d4d4779ce6
JanisNulle <janis.nulle@squalio.com>
2016-01-19 17:07:28 +0200

    Revert bat

--- runTests.bat
+++ runTests.bat
@@ -1,3 +1,8 @@
 cd %~dp0
 java -cp ".;lib/*;bin/" org.testng.TestNG testng.xml
+git checkout master
+git add .
+git commit -m "Automated test run"
+git remote set-url origin https://github.com/JanisNulle/ep119.git
+git push -u origin master
 exit /b 0

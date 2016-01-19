e27d025f6a7045e32a108ee7f8960cbe205afdbe
JanisNulle <janis.nulle@squalio.com>
2016-01-19 15:59:14 +0200

    Automated test run

--- runTests.bat
+++ runTests.bat
@@ -4,6 +4,6 @@ java -cp ".;lib/*;bin/" org.testng.TestNG testng.xml
 git checkout master
 git add .
 git commit -m "Automated test run"
-git remote add origin https://github.com/JanisNulle/ep119.git
+git remote set-url origin git@github.com/JanisNulle/ep119.git
 git push -u origin master
 exit /b 0
